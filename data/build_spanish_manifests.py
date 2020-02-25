d36cfce1abc7a8778e73d51696242917159643cc
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2020-02-25 17:20:57 +0100

    pool

--- data/build_spanish_manifests.py
+++ data/build_spanish_manifests.py
@@ -1,4 +1,5 @@
 import json
+import multiprocessing
 import os
 import subprocess
 import gzip
@@ -8,6 +9,7 @@ from tqdm import tqdm
 from utils import create_manifest
 
 SAMPLE_RATE = 16_000
+
 
 def read_jsonl(file, mode="b", limit=None, num_to_skip=0):
     assert any([mode == m for m in ["b", "t"]])
@@ -20,39 +22,52 @@ def read_jsonl(file, mode="b", limit=None, num_to_skip=0):
                 break
             yield json.loads(line.decode("utf-8") if mode == "b" else line)
 
+
 def _preprocess_transcript(phrase):
     return phrase.strip().lower()
 
+
 def process_example(wav_dir, txt_dir, audio_file, text):
-    audio_file_name = audio_file.split('/')[-1]
+    audio_file_name = audio_file.split("/")[-1]
     audio_file_name_no_suffix = os.path.splitext(audio_file_name)[0]
-    wav_recording_path = os.path.join(wav_dir, audio_file_name_no_suffix + '.wav')
-    subprocess.call(["sox {}  -r {} -b 16 -c 1 {}".format(audio_file, str(SAMPLE_RATE),
-                                                          wav_recording_path)], shell=True)
+    wav_recording_path = os.path.join(wav_dir, audio_file_name_no_suffix + ".wav")
+    subprocess.call(
+        [
+            "sox {}  -r {} -b 16 -c 1 {}".format(
+                audio_file, str(SAMPLE_RATE), wav_recording_path
+            )
+        ],
+        shell=True,
+    )
     # process transcript
-    transcript_file = os.path.join(txt_dir, audio_file_name_no_suffix + '.txt')
+    transcript_file = os.path.join(txt_dir, audio_file_name_no_suffix + ".txt")
 
     with open(transcript_file, "wb") as f:
-        f.write(_preprocess_transcript(text).encode('utf-8'))
+        f.write(_preprocess_transcript(text).encode("utf-8"))
         f.flush()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from pathlib import Path
 
     base_path = os.path.join(os.environ["HOME"], "data/asr_data/SPANISH")
 
-    split_type = 'train'
+    split_type = "train"
     wav_dir = os.path.join(base_path, split_type, "wav")
     txt_dir = os.path.join(base_path, split_type, "txt")
     Path(wav_dir).mkdir(parents=True, exist_ok=True)
     Path(txt_dir).mkdir(parents=True, exist_ok=True)
 
-
-
-    for audio_file,text in tqdm(read_jsonl(base_path+'/spanish_%s.jsonl'%split_type)):
-        audio_file = base_path+audio_file
+    def funfun(audio_file_text):
+        audio_file, text = audio_file_text
+        audio_file = base_path + audio_file
         process_example(wav_dir, txt_dir, audio_file, text)
 
-    create_manifest(wav_dir, 'spanish_' + split_type + '_manifest.csv',
-                    1, 30)
+    with multiprocessing.Pool(processes=50) as p:
+        result = list(
+            p.imap_unordered(
+                funfun, tqdm(read_jsonl(base_path + "/spanish_%s.jsonl" % split_type)),
+            )
+        )
 
+    create_manifest(wav_dir, "spanish_" + split_type + "_manifest.csv", 1, 30)
